0e05d66842efcd13a3fedbe1de16dd7c0ae9fa05
Jhon Birreo <jbirreo.n96@gmail.com>
2024-10-10 21:12:50 -0500

    feat: refactor route

--- lib/core/config/routes/app_router.dart
+++ lib/core/config/routes/app_router.dart
@@ -28,45 +28,48 @@ final appRouter = [
     parentNavigatorKey: navigationKey,
     builder: (context, state) => CreateTaskScreen(key: state.pageKey),
   ),
-  StatefulShellRoute.indexedStack(
-      builder: (context, state, child) =>
-          NavigationMenuScreen(navigationShell: child),
-      branches: <StatefulShellBranch>[
-        StatefulShellBranch(
-          navigatorKey: _rootNavigatorHome,
-          routes: [
-            GoRoute(
-              path: RouteLocation.home,
-              builder: (context, state) => HomeScreen(key: state.pageKey),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          navigatorKey: _rootNavigatorOrder,
-          routes: [
-            GoRoute(
-              path: RouteLocation.order,
-              builder: (context, state) => OrderScreen(key: state.pageKey),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          navigatorKey: _rootNavigatorProfile,
-          routes: [
-            GoRoute(
-              path: RouteLocation.profile,
-              builder: (context, state) => ProfileScreen(key: state.pageKey),
-            ),
-          ],
-        ),
-        StatefulShellBranch(
-          navigatorKey: _rootNavigatorFavorite,
-          routes: [
-            GoRoute(
-              path: RouteLocation.favorite,
-              builder: (context, state) => FavoriteScreen(key: state.pageKey),
-            ),
-          ],
-        )
-      ])
+  navigationBarRoute,
 ];
+
+final navigationBarRoute = StatefulShellRoute.indexedStack(
+  builder: (context, state, child) =>
+      NavigationMenuScreen(navigationShell: child),
+  branches: <StatefulShellBranch>[
+    StatefulShellBranch(
+      navigatorKey: _rootNavigatorHome,
+      routes: [
+        GoRoute(
+          path: RouteLocation.home,
+          builder: (context, state) => HomeScreen(key: state.pageKey),
+        ),
+      ],
+    ),
+    StatefulShellBranch(
+      navigatorKey: _rootNavigatorOrder,
+      routes: [
+        GoRoute(
+          path: RouteLocation.order,
+          builder: (context, state) => OrderScreen(key: state.pageKey),
+        ),
+      ],
+    ),
+    StatefulShellBranch(
+      navigatorKey: _rootNavigatorProfile,
+      routes: [
+        GoRoute(
+          path: RouteLocation.profile,
+          builder: (context, state) => ProfileScreen(key: state.pageKey),
+        ),
+      ],
+    ),
+    StatefulShellBranch(
+      navigatorKey: _rootNavigatorFavorite,
+      routes: [
+        GoRoute(
+          path: RouteLocation.favorite,
+          builder: (context, state) => FavoriteScreen(key: state.pageKey),
+        ),
+      ],
+    )
+  ],
+);
